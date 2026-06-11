0d9b535b118219c93081bee8aa30326a4f1ab1c6
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-06-11 15:22:11 +0700

    Memperbaiki metode inorder untuk mencetak elemen pohon biner

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -81,4 +81,12 @@ class BinaryTree
             cout << "Tree is empty" << endl;
             return;
         }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 };
